e969f363a99f330114da8b2c70ea9b8ec7c4b061
unknown <badaev.dmi@gmail.com>
2022-05-14 16:44:32 +0300

    task1 2.0

--- task1/task1.py
+++ task1/task1.py
@@ -1,25 +1,25 @@
-#from sys import argv
-#script, n, m = argv
-n = int(input("Введите длину массива: "))
-m = int(input("Введите длину интервала: "))
-#Проверка на корректность значений
-if n != m:
-    if n > 0 and m > 0:
-        if n % 2 == 0 and m % 2 == 0:
-            print("Оба значения не могут быть чётными")
-        else:
-            interval = list(range(1, n+1))
-            array = interval * 10000000
-            #k - индекс элемента массива
-            i = 1
-            k = 1
-            while array[k] != 1:
-                i += 1
-                k = i * m - 1
-            else:
-                lst = array[0:k:m]
-                print(lst);
-    else:
-        print("Оба занчения должны быть больше нуля")
-else:
-    print("Длина массива не может быть равна длине интервала");
+try:
+	from sys import argv
+	script, n, m = argv
+	n = int(n)
+	m = int(m)
+	if n > 0 and m > 0:
+    		target = n + 1
+    		dif = m - 1
+    		array=[]
+    		k = 1
+    		while k != target:
+      			array.append(k)
+      			k = k + dif
+      			if k > target:
+        			#k = k - dif
+        			#k = (dif - (n - k))
+        			k = k - n
+	else:print("Оба занчения должны быть больше нуля.")
+	answer = ''.join(str(e) for e in array)
+	print(answer);
+except Exception:
+	print("Введите корректное значение.");
+
+
+
